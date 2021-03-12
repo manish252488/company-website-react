fb998a104c3eed209c00065d6765c4be68e6e7fa
Umang <umang@kplay.team>
2021-03-12 19:00:42 +0530

    Comments changed

--- src/config/authRoles.js
+++ src/config/authRoles.js
@@ -3,16 +3,17 @@
  *  [user role]: [role pages that can be accessed by this user]
  *
  * For Example:
- *  a user admin
- *  admin: [guest,sales,admin]
- *  sales: [guest, sales]
- *  can access the pages that have a config of guest, sales and admin,
- *  while the sales role can only access the pages with sale and guest config
+ *  admin: [admin,superuser]               // admin and sales department can access the pages with the config sales.
+ *  sales: [admin, sales,superuser]        // admin can access the pages with only admin config.
+ *  superuser: [superuser]                 // superuser can access all pages.
+ *  guest: [user,sales,admin,superuser]   //  guest pages can be access by all
+ *
+ *
  */
 
 const authRoles = {
-  user: ["user"],
+  user: ["user"], // the pages with user config can be accessed by roles: ["user"]
   user2: ["user2"],
-  guest: ["guest", "user", "user2"],
+  guest: ["guest", "user", "user2"], // the pages with guest config can be accessed by roles: [user,guest,admin]
 };
 export default authRoles;
