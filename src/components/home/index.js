3ca82fe3bd3a02dcfdba225420c14b252b3dd9bd
Umang <umang@kplay.team>
2021-03-13 12:29:12 +0530

    ADD: fade package

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 import "./index.less";
 import { useTranslation } from "react-i18next";
+import Fade from "react-reveal";
 const Home = (props) => {
   const { t, i18n } = useTranslation();
-  return (
-    <div
-      onClick={() =>
-        i18n.language === "en"
-          ? i18n.changeLanguage("ge")
-          : i18n.changeLanguage("en")
-      }
-    >
-      {t("home")}
-    </div>
-  );
+  return <div></div>;
 };
 export default Home;
